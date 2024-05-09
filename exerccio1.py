d7094fcb005c421084e189a31154182602106337
loterinia <00001114371385sp@al.educacao.sp.gov.br>
2024-05-09 11:08:53 -0300

    Observações

---
+++ exerccio1.py
@@ -0,0 +1,58 @@
+lerLetra = str.lower((input("Digite uma letra: ")))
+
+
+if lerLetra == 'a':
+    print("Avatar")
+elif lerLetra == 'b':
+    print("Batman")
+elif lerLetra =='c':
+    print("Cinderela")
+elif lerLetra == 'd':
+    print("De Repente 30")
+elif lerLetra == 'e':
+    print("Enrolados")
+elif lerLetra == 'f':
+    print("Frozen")
+elif lerLetra == 'g':
+    print("Garfield")
+elif lerLetra == 'h':
+    print("Hannah Montana")
+elif lerLetra =='i':
+    print("It a Coisa")
+elif lerLetra == 'j':
+    print("Jack")
+elif lerLetra == 'k':
+    print("Kadaver")
+elif lerLetra == 'l':
+    print("Luca")
+elif lerLetra == 'm':
+    print("Mulher Maravilha")
+elif lerLetra == 'n':
+    print("Na chuva")
+elif lerLetra == 'o':
+    print("Os Incríveis")
+elif lerLetra == 'p':
+    print("Procurando Nemo")
+elif lerLetra == 'q':
+    print("Quando As Luzes Se Apagam")
+elif lerLetra == 'r':
+    print("Rio")
+elif lerLetra == 's':
+    print("Sabor da Vida")
+elif lerLetra == 't':
+    print("Titanic")
+elif lerLetra == 'u':
+    print("Última Noite")
+elif lerLetra == 'v':
+    print("Viúva Negra")
+elif lerLetra == 'w':
+    print("Wendy")
+elif lerLetra == 'x':
+    print("X Games 3D")
+elif lerLetra == 'Y':
+    print("Yara")
+elif lerLetra == 'z':
+    print("Zé Colmeia")
+    
+else:
+    print("Não conheço nenhum filme com essa letra.")
